bcf03187b748b515a8f9301b383f3b9baf742362
Edoscoba <obisimon88@gmail.com>
2024-11-06 03:09:30 +0100

    App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Statistics from "./components/Statistics";
+import Transactions from "./components/Transactions";
+import User from "./components/User";
 
 function App() {
   return (
     <>
-      <h1 className="text-red-500">Hello World Please Subscribe</h1>
+      <Sidebar />
+      <main className="grid grid-cols-1 md:grid-cols-[auto_300px] lg:pl-[250px]">
+        {/* column 1 start */}
+        <section className="p-4 md:pb-16">
+          <Navbar />
+          <Statistics />
+          <Transactions />
+        </section>
+        {/* column  1 end */}
+        {/* column 2 start */}
+        <section className="p-4 pb-16 border-bordercolor lg:border-l-2">
+          <User />
+        </section>
+        {/* column 2 end */}
+      </main>
     </>
   );
 }
